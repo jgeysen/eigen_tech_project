dbe18a68f169d5e29f13c0978cb9bdb060c4d4eb
jgeysen <jori.geysen@anmut.co.uk>
2021-05-18 21:34:08 +0100

    move code from notebooks

---
+++ eigen_tech_project/inverted_index.py
@@ -0,0 +1,114 @@
+import re
+from operator import add
+from os import listdir
+from os.path import abspath, isfile, join
+
+import nltk
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+from eigen_tech_project.nlp_processing import SentenceProcessor
+
+
+class InvertedIndex:
+    def __init__(self, path):
+        self.path = path
+        self.sentence_splitter = nltk.data.load("tokenizers/punkt/english.pickle")
+        self.sentence_processor = SentenceProcessor
+
+    def __repr__(self):
+        """Returns representation of the DataLoader object."""
+        return "{}({!r})".format(self.__class__.__name__, self.path)
+
+    @property
+    def file_names(self):
+        """Returns representation of the DataLoader object."""
+        return [f for f in listdir(self.path) if isfile(join(self.path, f))]
+
+    @property
+    def raw_data(self):
+        """Returns representation of the DataLoader object."""
+
+        return [
+            (
+                int(re.sub("[^0-9]", "", f)),
+                open(abspath(join(self.path, f)), "r").read(),
+            )
+            for f in self.file_names
+        ]
+
+    @property
+    def sentences(self):
+        """Returns representation of the DataLoader object."""
+        data = []
+        for file in self.raw_data:
+            # split each file in sentences:
+            sentences = [
+                x for x in self.sentence_splitter.tokenize(file[1]) if len(x) > 1
+            ]
+            # list of sentence level dictionaries:
+            data.extend([(file[0], sentence) for sentence in sentences])
+
+        # add index:
+        ix = [(x,) for x in range(0, len(data))]
+        return list(map(add, ix, data))
+
+    @property
+    def processed_sentences(self):
+        """Returns representation of the DataLoader object."""
+        return [
+            sentence + (self.sentence_processor(sentence[2]).processed_sentence,)
+            for sentence in self.sentences
+        ]
+
+    @property
+    def inverted_index(self):
+        """Return list with the top x most occuring interesting words, with
+        following elements: (feature_id, occurence)."""
+        data = [x[3] for x in self.processed_sentences]
+        countvectorizer = CountVectorizer()
+
+        vectorizer = countvectorizer.fit(data)
+        doc_term_matrix = vectorizer.transform(data)
+
+        lemmas = vectorizer.get_feature_names()
+        frequencies = doc_term_matrix.sum(axis=0).tolist()[0]
+        occurences = [
+            doc_term_matrix[:, i].nonzero()[0].tolist() for i in range(0, len(lemmas))
+        ]
+
+        return list(zip(lemmas, frequencies, occurences))
+
+    #     def _sentence_mapper(self, sentence_ids):
+    #         """Return list with the top x most occuring interesting words, with following elements: (feature_id, occurence)."""
+    #         return [z for (x,y,z) in self.sentences if x.isin(sentence_ids)]
+
+    #     def _document_mapper(self, sentence_ids):
+    #         """Return list with the top x most occuring interesting words, with following elements: (feature_id, occurence)."""
+    #         return [y for (x,y,z) in self.sentences if x in sentence_ids]
+
+    @property
+    def solution(self):
+        df_input = pd.DataFrame(
+            self.sentences, columns=["index", "document", "sentence"]
+        )
+        df_output = pd.DataFrame(
+            self.inverted_index, columns=["lemma", "frequency", "sentences"]
+        )
+
+        # map sentence ids to documents:
+        df_output["documents"] = [
+            set(df_input.iloc[x].document) for x in df_output.sentences
+        ]
+
+        # map sentence ids to strings:
+        df_output["sentences"] = [
+            df_input.iloc[x].sentence.tolist() for x in df_output.sentences
+        ]
+
+        df_output = df_output.sort_values("frequency", ascending=False).reset_index(
+            drop=True
+        )
+        df_output.to_csv("output.csv", index=False)
+
+        return df_output
---
+++ eigen_tech_project/nlp_processing.py
@@ -0,0 +1,86 @@
+import nltk
+import requests
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
+
+class SentenceProcessor:
+    """split the document into sentences and tokenize each sentence."""
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.tokenizer = nltk.RegexpTokenizer(r"\w+")
+        self.lemmatizer = Lemmatizer()
+        self.stopwords = nltk.corpus.stopwords.words("english")
+        self.common_words_url = "https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt"
+
+    def __repr__(self):
+        """Returns representation of the DataLoader object."""
+        return "{}({!r})".format(self.__class__.__name__, self.sentence)
+
+    @property
+    def tokenized_sentence(self):
+        """Return a list of sublists with tokens."""
+        # tokenization in each sentences
+        return self.tokenizer.tokenize(self.sentence.lower())
+
+    @property
+    def lemmatized_sentence(self):
+        """Return a list of sublists with lemmas."""
+        return self.lemmatizer.lemmas(self.tokenized_sentence)
+
+    def remove_stopwords(self, text):
+        """return WORDNET POS compliance to WORDNET lemmatization (a,n,r,v)"""
+        common_words = requests.get(self.common_words_url).text.split()
+        noise = self.stopwords + common_words
+        return [w for w in text if w not in noise and w.isalpha()]
+
+    @property
+    def lemmatized_sentence_no_stop(self):
+        """Return a list of sublists with interesting lemmas.
+
+        Ergo: stopword and non-alphabetical removal.
+        """
+        return self.remove_stopwords(self.lemmatized_sentence)
+
+    @property
+    def processed_sentence(self):
+        """Return a single sentence."""
+        return " ".join(self.lemmatized_sentence_no_stop)
+
+
+class Lemmatizer:
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+
+    def __repr__(self):
+        """Returns representation of the DataLoader object."""
+        return "{}({!r})".format(self.__class__.__name__, "")
+
+    def get_wordnet_pos(self, treebank_tag):
+        """return WORDNET POS compliance to WORDNET lemmatization (a,n,r,v)"""
+        if treebank_tag.startswith("J"):
+            return wordnet.ADJ
+        elif treebank_tag.startswith("V"):
+            return wordnet.VERB
+        elif treebank_tag.startswith("N"):
+            return wordnet.NOUN
+        elif treebank_tag.startswith("R"):
+            return wordnet.ADV
+        else:
+            # As default pos in lemmatization is Noun
+            return wordnet.NOUN
+
+    def get_lemma(self, word_postag_combo):
+        """return WORDNET POS compliance to WORDNET lemmatization (a,n,r,v)"""
+        return self.lemmatizer.lemmatize(
+            word_postag_combo[0], self.get_wordnet_pos(word_postag_combo[1])
+        )
+
+    def lemmas(self, tokenized_sentence):
+        """return WORDNET POS compliance to WORDNET lemmatization (a,n,r,v)"""
+        # find the pos tagging for each tokens [('What', 'WP'), ('can', 'MD'), ('I', 'PRP') ....
+        pos_tokens = nltk.pos_tag(tokenized_sentence)
+        # lemmatization using pos tags
+        lemmas = [self.get_lemma(word_tag_combo) for word_tag_combo in pos_tokens]
+        return lemmas
