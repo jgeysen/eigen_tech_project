638708c14c2adb0670b89536317203513a798669
jgeysen <jori.geysen@anmut.co.uk>
2021-05-18 21:41:53 +0100

    move code from notebooks

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     dependency_links=[],
     description="Project made during the interview process for a software engineering position at Eigen Technologies.",
-    install_requires=["nltk", "pandas", "scikit-learn", "numpy"],
+    install_requires=["nltk", "pandas", "scikit-learn"],
     license="MIT license",
     keywords="eigen_tech_project",
     name="eigen_tech_project",
