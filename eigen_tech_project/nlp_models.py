688d40c10d34eef4cf776e9c0fb4ae5cf0ecac7e
jgeysen <jori.geysen@anmut.co.uk>
2021-05-20 00:26:15 +0100

    mend

--- eigen_tech_project/nlp_models.py
+++ eigen_tech_project/nlp_models.py
@@ -1,24 +1,26 @@
-# try:  # pragma: no cover
-#     nltk.data.load("tokenizers/punkt/english.pickle")
-# except LookupError:  # pragma: no cover
-#     nltk.download("punkt")
-#
-# try:  # pragma: no cover
-#     stopwords = nltk.corpus.stopwords.words("english")
-# except LookupError:  # pragma: no cover
-#     nltk.download("stopwords")
-#
-# try:  # pragma: no cover
-#     pos = nltk.data.load(
-#         "taggers/averaged_perceptron_tagger/averaged_perceptron_tagger.pickle"
-#     )
-# except LookupError:  # pragma: no cover
-#     nltk.download("averaged_perceptron_tagger")
-#
-# try:  # pragma: no cover
-#     from nltk.corpus import wordnet  # noqa
-# except LookupError:  # pragma: no cover
-#     nltk.download("wordnet")
+import nltk
+
+try:  # pragma: no cover
+    nltk.data.load("tokenizers/punkt/english.pickle")
+except LookupError:  # pragma: no cover
+    nltk.download("punkt")
+
+try:  # pragma: no cover
+    stopwords = nltk.corpus.stopwords.words("english")
+except LookupError:  # pragma: no cover
+    nltk.download("stopwords")
+
+try:  # pragma: no cover
+    pos = nltk.data.load(
+        "taggers/averaged_perceptron_tagger/averaged_perceptron_tagger.pickle"
+    )
+except LookupError:  # pragma: no cover
+    nltk.download("averaged_perceptron_tagger")
+
+try:  # pragma: no cover
+    from nltk.corpus import wordnet  # noqa
+except LookupError:  # pragma: no cover
+    nltk.download("wordnet")
 
 
 # nltk.download("wordnet")
