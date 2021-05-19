5f4c9656acd048464523479b9fadeab5e4823de4
jgeysen <jori.geysen@anmut.co.uk>
2021-05-19 23:43:22 +0100

    mend

--- eigen_tech_project/nlp_models.py
+++ eigen_tech_project/nlp_models.py
@@ -1,11 +1,8 @@
 import nltk
 
-# import the nlp language model:
 try:  # pragma: no cover
     nltk.data.load("tokenizers/punkt/english.pickle")
-    # nltk_data = nltk.data.load("corpora/wordnet.zip/wordnet/")
 except LookupError:  # pragma: no cover
-    # nltk.download("wordnet")
     nltk.download("punkt")
 
 try:  # pragma: no cover
@@ -13,10 +10,14 @@ try:  # pragma: no cover
 except LookupError:  # pragma: no cover
     nltk.download("stopwords")
 
-
 try:  # pragma: no cover
     pos = nltk.data.load(
         "taggers/averaged_perceptron_tagger/averaged_perceptron_tagger.pickle"
     )
 except LookupError:  # pragma: no cover
     nltk.download("averaged_perceptron_tagger")
+
+try:  # pragma: no cover
+    wordnet = nltk.data.load("corpora/wordnet.zip/wordnet/")
+except LookupError:  # pragma: no cover
+    nltk.download("wordnet")
