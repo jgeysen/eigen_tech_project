c620a0d80e1b3e9a65f3774ddfb7ab5f2a84226c
jgeysen <jori.geysen@anmut.co.uk>
2021-05-21 00:10:43 +0100

    add requests library

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     dependency_links=[],
     description="Project made during the interview process for a software engineering position at Eigen Technologies.",
-    install_requires=["nltk", "pandas", "scikit-learn", "cached-property"],
+    install_requires=["nltk", "pandas", "scikit-learn", "cached-property", "requests"],
     keywords="eigen_tech_project",
     name="eigen_tech_project",
     packages=find_packages(include=["eigen_tech_project"]),
